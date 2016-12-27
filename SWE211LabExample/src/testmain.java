0f857f83525d59e440d491035222555eb678ec68
Ali Alotaibi <asta828@gmail.com>
2016-12-27 22:03:46 +0300

    updated main method

--- SWE211LabExample/src/testmain.java
+++ SWE211LabExample/src/testmain.java
@@ -2,6 +2,10 @@
 public class testmain {
 
 	public static void main(String[] args) {
+		int x=5;
+		if(5>x){
+			System.out.println("Wrong");
+		}
 		System.out.println("Testing git for SWE 211");
 	}
 
